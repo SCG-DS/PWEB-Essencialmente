be555cf8b96067a20c34c25a2fb643224bf63684
SCG-DS <samuelcgds@gmail.com>
2025-12-01 19:49:40 -0300

    Add sex options

--- IMC/models.py
+++ IMC/models.py
@@ -3,9 +3,15 @@ from stdimage import StdImageField
 
 # Create your models here.
 class Avaliar_IMC(models.Model):
+    SEXOS = [
+        ('MASC', 'Masculino'),
+        ('FEMI', 'Feminino'),
+        ('NAOI', 'Não Informado')
+    ]
+
     cpf = models.CharField('CPF', null=False, blank=False, max_length=11, unique=True, primary_key=True)
     nome = models.CharField('Nome', null=False, blank=False, max_length=70)
-    sexo = models.CharField('Sexo', null=False, blank=False, max_length=9)
+    sexo = models.CharField('Sexo', null=False, blank=False, max_length=9, choices=SEXOS)
     dataNascimento = models.DateField('Data de Nascimento', null=False, blank=False)
     escola = models.CharField('Escola', null=False, blank=False, max_length=100)
     altura = models.FloatField('Altura', null=False, blank=False)
